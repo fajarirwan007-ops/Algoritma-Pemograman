2e57e4e12a3b950dbcea7be7f696bae2beb55fb7
Fajar Irwansyah <fajarirwan007@gmail.com>
2026-09-17 17:41:06 +0700

    Add files via upload
    Pertemuan pertama

---
+++ Pertemuan Pertama latihan1.py
@@ -0,0 +1,17 @@
+#data persegi
+persegi = 30
+bilangan = 4
+keliling = "" 
+
+#data tampil
+print ("-- Data sebelum di hitung luas --")
+print ("Panjang Persegi :",persegi)
+print ("Keliling Persegi :",keliling)
+
+#menghitung persegi
+hasilnya = persegi*bilangan
+
+#data hasilnya
+print ("\n-- Data Hasil di hitung luas--")
+print ("Panjang Persegi :",persegi)
+print ("Keliling Persegi :",hasilnya)
